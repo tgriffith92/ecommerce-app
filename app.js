1dc3c81d16fdea4272ca3ad53d9d8f0eb5880441
Terence <terencegriff92@gmail.com>
2020-06-13 23:36:36 -0400

    create the express server

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('Hello!');
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+	res.send('Hi there!');
+});
+
+app.listen(3000, () => {
+	console.log('Listening');
+});
