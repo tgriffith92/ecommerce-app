cc51cc6442be5818b75487336ce67b8aa81f4d58
Terence <terencegriff92@gmail.com>
2020-06-14 00:04:37 -0400

    add get and post methods

--- app.js
+++ app.js
@@ -3,7 +3,20 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-	res.send('Hi there!');
+	res.send(`
+        <div>
+            <form method="POST">
+                <input name="email" placeholder="email" />
+                <input name="password" placeholder="password" />
+                <input name="passwordConfirmation" placeholder="password confirmation" />
+                <button>Sign Up</button>
+            </form>
+        </div>
+    `);
+});
+
+app.post('/', (req, res) => {
+	res.send('Account created!');
 });
 
 app.listen(3000, () => {
