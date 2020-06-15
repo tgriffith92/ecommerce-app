bfa554945e1efaa83db4df81b22f76b1084deb5f
Terence <terencegriff92@gmail.com>
2020-06-15 15:42:59 -0400

    added signout functionality

--- app.js
+++ app.js
@@ -12,9 +12,10 @@ app.use(
 	})
 );
 
-app.get('/', (req, res) => {
+app.get('/signup', (req, res) => {
 	res.send(`
-        <div>
+		<div>
+			Your id is ${req.session.userId}
             <form method="POST">
                 <input name="email" placeholder="email" />
                 <input name="password" placeholder="password" />
@@ -25,7 +26,7 @@ app.get('/', (req, res) => {
     `);
 });
 
-app.post('/', async (req, res) => {
+app.post('/signup', async (req, res) => {
 	const { email, password, passwordConfirmation } = req.body;
 
 	const existingUser = await usersRepo.getOneBy({ email });
@@ -39,7 +40,26 @@ app.post('/', async (req, res) => {
 
 	const user = await usersRepo.create({ email, password });
 
+	req.session.userId = user.id;
+
 	res.send('Account created!');
+});
+
+app.get('/signout', (req, res) => {
+	req.session = null;
+	res.send('You are logged out!');
+});
+
+app.get('/signin', (req, res) => {
+	res.send(`
+	<div>
+	<form method="POST">
+		<input name="email" placeholder="email" />
+		<input name="password" placeholder="password" />
+		<button>Sign In</button>
+	</form>
+</div>
+	`);
 });
 
 app.listen(3000, () => {
