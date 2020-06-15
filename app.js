98e3ce84c74f65cd893b0f0227890cd659d4c7fa
Terence <terencegriff92@gmail.com>
2020-06-15 14:56:11 -0400

    added the cookie session library

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieSession = require('cookie-session');
 const usersRepo = require('./repositories/users');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+	cookieSession({
+		keys: [ 'nuwiehfsndfuiwefew' ]
+	})
+);
 
 app.get('/', (req, res) => {
 	res.send(`
@@ -30,6 +36,8 @@ app.post('/', async (req, res) => {
 	if (password !== passwordConfirmation) {
 		return res.send('Passwords must match!');
 	}
+
+	const user = await usersRepo.create({ email, password });
 
 	res.send('Account created!');
 });
