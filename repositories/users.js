c61eed129dd30fdc7df1d1930d069459e77a26a7
Terence <terencegriff92@gmail.com>
2020-06-14 21:16:35 -0400

    get a user by id

--- repositories/users.js
+++ repositories/users.js
@@ -39,16 +39,19 @@ class UsersRepository {
 	randomId() {
 		return crypto.randomBytes(4).toString('hex');
 	}
+
+	async getOne(id) {
+		const records = await this.getAll();
+		return records.find((record) => record.id === id);
+	}
 }
 
 const test = async () => {
 	const repo = new UsersRepository('users.json');
 
-	await repo.create({ email: 'test@test.com', password: 'password' });
+	const user = await repo.getOne('97126b02');
 
-	const users = await repo.getAll();
-
-	console.log(users);
+	console.log(user);
 };
 
 test();
