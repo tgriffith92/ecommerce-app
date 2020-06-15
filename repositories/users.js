4bfe85de2fe0fb75f3e7e84d4bdd89909209a774
Terence <terencegriff92@gmail.com>
2020-06-14 21:20:26 -0400

    add code to delete a user by id

--- repositories/users.js
+++ repositories/users.js
@@ -44,14 +44,19 @@ class UsersRepository {
 		const records = await this.getAll();
 		return records.find((record) => record.id === id);
 	}
+
+	async delete(id) {
+		const records = await this.getAll();
+		const filteredRecords = records.filter((record) => record.id !== id);
+
+		await this.writeAll(filteredRecords);
+	}
 }
 
 const test = async () => {
 	const repo = new UsersRepository('users.json');
 
-	const user = await repo.getOne('97126b02');
-
-	console.log(user);
+	await repo.delete('345a4e98');
 };
 
 test();
