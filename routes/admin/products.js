ce4cc58e38200d9824aa346b133afc58c956aa9c
Terence <terencegriff92@gmail.com>
2020-06-17 16:04:07 -0400

    added the initial code to add a product with an image

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -17,7 +17,11 @@ router.post('/admin/products/new', [
 	requirePrice,
 	(req, res) => {
 		const errors = validationResult(req);
-		console.log(errors);
+
+		req.on('data', (data) => {
+			console.log(data.toString());
+		});
+
 		res.send('submitted');
 	}
 ]);
