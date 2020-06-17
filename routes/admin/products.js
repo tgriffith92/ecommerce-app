13948fc321d57760b588a864c02e176612ef78c5
Terence <terencegriff92@gmail.com>
2020-06-17 18:24:47 -0400

    added error handling code for a new product

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -15,10 +15,16 @@ router.get('/admin/products/new', (req, res) => {
 	res.send(productsNewTemplate({}));
 });
 
-router.post('/admin/products/new', [ requireTitle, requirePrice ], upload.single('image'), (req, res) => {
+router.post('/admin/products/new', [ requireTitle, requirePrice ], upload.single('image'), async (req, res) => {
 	const errors = validationResult(req);
 
-	console.log(req.file);
+	if (!errors.isEmpty()) {
+		return res.send(productsNewTemplate({ errors }));
+	}
+
+	const image = req.file.buffer.toString('base64');
+	const { title, price } = req.body;
+	await productsRepo.create({ title, price, image });
 
 	res.send('submitted');
 });
