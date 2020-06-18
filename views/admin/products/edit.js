a507d4457e13a644146a0f2ec20f9db05b5f2781
Terence <terencegriff92@gmail.com>
2020-06-18 14:50:27 -0400

    added code to update a product in the browser and redirect to products page

--- views/admin/products/edit.js
+++ views/admin/products/edit.js
@@ -3,7 +3,7 @@ const layout = require('../layout');
 module.exports = ({ product }) => {
 	return layout({
 		content: `
-            <form>
+            <form method="POST">
                 <input name="title" value="${product.title}"/>
                 <input name="price" value="${product.price}"/>
                 <input name="image" type="file"/>
